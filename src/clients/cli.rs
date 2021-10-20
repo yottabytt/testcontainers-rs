87a5bd3f707cfbe5a1403a87131501f4e58fb080
dparthiban <dparthiban@etsy.com>
2021-10-20 15:41:34 -0400

    debug logs

--- src/clients/cli.rs
+++ src/clients/cli.rs
@@ -45,6 +45,11 @@ impl Cli {
 
         let output = command.output().expect("Failed to execute docker command");
 
+        if !output.status.success() {
+            log::debug!("stdout log: {:?}", String::from_utf8_lossy(&output.stdout));
+            log::debug!("stdout log: {:?}", String::from_utf8_lossy(&output.stderr));
+        }
+
         assert!(output.status.success(), "failed to start container");
         let container_id = String::from_utf8(output.stdout)
             .expect("output is not valid utf8")
