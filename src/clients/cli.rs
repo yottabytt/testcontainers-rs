a453eefded91d25aa540b82af4494577a5a18200
dparthiban <dparthiban@etsy.com>
2021-10-20 15:50:21 -0400

    panic msg

--- src/clients/cli.rs
+++ src/clients/cli.rs
@@ -45,12 +45,12 @@ impl Cli {
 
         let output = command.output().expect("Failed to execute docker command");
 
-        if !output.status.success() {
-            log::debug!("stdout log: {:?}", String::from_utf8_lossy(&output.stdout));
-            log::debug!("stdout log: {:?}", String::from_utf8_lossy(&output.stderr));
-        }
-
-        assert!(output.status.success(), "failed to start container");
+        assert!(
+            output.status.success(),
+            "failed to start container. {:?} {:?}",
+            String::from_utf8_lossy(&output.stdout),
+            String::from_utf8_lossy(&output.stderr)
+        );
         let container_id = String::from_utf8(output.stdout)
             .expect("output is not valid utf8")
             .trim()
