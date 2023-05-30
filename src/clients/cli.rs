f186d6d1589c7bdca9dcef6799e4a427667ac93d
michael <mpgarate@gmail.com>
2023-05-30 21:30:45 +0000

    print error output

--- src/clients/cli.rs
+++ src/clients/cli.rs
@@ -45,7 +45,12 @@ impl Cli {
 
         let output = command.output().expect("Failed to execute docker command");
 
-        assert!(output.status.success(), "failed to start container");
+        assert!(
+            output.status.success(),
+            "failed to start container: {:?}",
+            output
+        );
+
         let container_id = String::from_utf8(output.stdout)
             .expect("output is not valid utf8")
             .trim()
